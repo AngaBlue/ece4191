74c95cf080fac72de016311b191f43d0d0af0047
AngaBlue <contact@anga.blue>
2025-08-21 00:57:58 +1000

    refactor: config & setup

---
+++ firmware/include/config.h
@@ -0,0 +1,16 @@
+#include "esp_camera.h"
+
+/**
+ * Miscellaneous
+ */
+static const char *NAME = "ESP32S3-R06";
+static const char *AP_SSID = NAME;
+static const char *AP_PASS = "12345678";
+
+/**
+ * Camera
+ */
+#define FRAME_SIZE FRAMESIZE_VGA
+#define JPEG_QUALITY 10
+#define FRAME_BUFFER_COUNT 2
+
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,30 +2,10 @@
 #include <WiFi.h>
 #include "esp_camera.h"
 #include <WebServer.h>
-
-#ifndef CAM_PIN_XCLK
-#error "Camera pins not defined. Ensure scripts/camera_pins.py sets CAM_PIN_* macros."
-#endif
-
-// ---- Quick test as AP (no router needed) ----
-static const char *AP_SSID = "S3CamTest";
-static const char *AP_PASS = "12345678"; // 8+ chars required by ESP AP
+#include "pins.h"
+#include "config.h"
 
 WebServer server(80);
-
-// Minimal homepage
-static const char INDEX_HTML[] PROGMEM = R"HTML(
-<!doctype html><html>
-<head><meta name=viewport content="width=device-width,initial-scale=1">
-<title>ESP32-S3 Cam</title>
-<style>body{font-family:system-ui;margin:20px}img{max-width:100%;height:auto;border:1px solid #ccc}</style>
-</head>
-<body>
-<h3>ESP32-S3 Camera</h3>
-<p><a href="/jpg">Take still</a></p>
-<img src="/stream">
-</body></html>
-)HTML";
 
 // Single JPEG endpoint
 void handle_jpg()
@@ -83,66 +63,61 @@ void handle_stream()
   }
 }
 
-// Camera init
-bool init_camera()
+void hang()
+{
+  while (true)
+    delay(1000);
+}
+
+void init_camera()
 {
   camera_config_t c = {};
   c.ledc_channel = LEDC_CHANNEL_0;
   c.ledc_timer = LEDC_TIMER_0;
-  c.pin_d0 = CAM_PIN_D0;
-  c.pin_d1 = CAM_PIN_D1;
-  c.pin_d2 = CAM_PIN_D2;
-  c.pin_d3 = CAM_PIN_D3;
-  c.pin_d4 = CAM_PIN_D4;
-  c.pin_d5 = CAM_PIN_D5;
-  c.pin_d6 = CAM_PIN_D6;
-  c.pin_d7 = CAM_PIN_D7;
-  c.pin_xclk = CAM_PIN_XCLK;
-  c.pin_pclk = CAM_PIN_PCLK;
-  c.pin_vsync = CAM_PIN_VSYNC;
-  c.pin_href = CAM_PIN_HREF;
-  c.pin_sccb_sda = CAM_PIN_SIOD;
-  c.pin_sccb_scl = CAM_PIN_SIOC;
-  c.pin_pwdn = CAM_PIN_PWDN;
-  c.pin_reset = CAM_PIN_RESET;
+  c.pin_d0 = PIN_CAM_D0;
+  c.pin_d1 = PIN_CAM_D1;
+  c.pin_d2 = PIN_CAM_D2;
+  c.pin_d3 = PIN_CAM_D3;
+  c.pin_d4 = PIN_CAM_D4;
+  c.pin_d5 = PIN_CAM_D5;
+  c.pin_d6 = PIN_CAM_D6;
+  c.pin_d7 = PIN_CAM_D7;
+  c.pin_xclk = PIN_CAM_XCLK;
+  c.pin_pclk = PIN_CAM_PCLK;
+  c.pin_vsync = PIN_CAM_VSYNC;
+  c.pin_href = PIN_CAM_HREF;
+  c.pin_sccb_sda = PIN_CAM_SIOD;
+  c.pin_sccb_scl = PIN_CAM_SIOC;
+  c.pin_pwdn = PIN_CAM_PWDN;
+  c.pin_reset = PIN_CAM_RESET;
   c.xclk_freq_hz = 20000000;
-  c.frame_size = FRAMESIZE_VGA;
-  c.jpeg_quality = 12;
-  c.fb_count = 4;
+  c.frame_size = FRAME_SIZE;
+  c.jpeg_quality = JPEG_QUALITY;
+  c.fb_count = FRAME_BUFFER_COUNT;
   c.fb_location = CAMERA_FB_IN_PSRAM;
   c.grab_mode = CAMERA_GRAB_LATEST;
   c.pixel_format = PIXFORMAT_JPEG;
 
   esp_err_t err = esp_camera_init(&c);
   if (err != ESP_OK)
-    return false;
+  {
+    Serial.println("Camera: init failed");
+    hang();
+  }
 
   sensor_t *s = esp_camera_sensor_get();
-  return true;
+  Serial.println("Camera: init successful");
 }
 
-void setup()
+void init_ap()
 {
-  Serial.begin(115200);
-  delay(200);
-  Serial.println("Booted");
-  if (!init_camera())
-  {
-    Serial.println("Camera init failed");
-    for (;;)
-      delay(1000);
-  }
-  Serial.println("Camera init OK");
-
-  // Start AP (fastest path)
   WiFi.mode(WIFI_AP);
   WiFi.setSleep(false);
-  bool ok = WiFi.softAP(AP_SSID, AP_PASS);
+  bool ok = WiFi.softAP(AP_SSID, AP_PASS, 1, 0, 1);
   if (!ok)
   {
-    Serial.println("AP start failed");
-    for (;;)
-      delay(1000);
+    Serial.println("AP: init failed");
+    hang();
   }
   Serial.print("AP SSID: ");
   Serial.println(AP_SSID);
@@ -150,9 +125,16 @@ void setup()
   Serial.println(AP_PASS);
   Serial.print("AP IP:   ");
   Serial.println(WiFi.softAPIP());
+}
 
-  server.on("/", []()
-            { server.send_P(200, "text/html", INDEX_HTML); });
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("Booted!");
+
+  init_camera();
+  init_ap();
+
   server.on("/jpg", handle_jpg);
   server.on("/stream", HTTP_GET, handle_stream);
   server.begin();
