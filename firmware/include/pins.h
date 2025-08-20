a5cbbd189d783a5f693c79c70794ec79241c9fa2
AngaBlue <contact@anga.blue>
2025-08-21 00:35:03 +1000

    feat: pin defines

---
+++ firmware/include/pins.h
@@ -0,0 +1,55 @@
+/**
+ * Camera
+ */
+#define PIN_CAM_PWDN -1 // not connected
+#define PIN_CAM_RESET -1 // not connected
+
+#define PIN_CAM_SIOD 4
+#define PIN_CAM_SIOC 5
+
+#define PIN_CAM_VSYNC 6
+#define PIN_CAM_HREF 7
+#define PIN_CAM_XCLK 15
+#define PIN_CAM_PCLK 13
+
+#define PIN_CAM_D7 16 // CAM_Y9
+#define PIN_CAM_D6 17 // CAM_Y8
+#define PIN_CAM_D5 18 // CAM_Y7
+#define PIN_CAM_D4 12 // CAM_Y6
+#define PIN_CAM_D3 10 // CAM_Y5
+#define PIN_CAM_D2 8  // CAM_Y4
+#define PIN_CAM_D1 9  // CAM_Y3
+#define PIN_CAM_D0 11 // CAM_Y2
+
+/**
+ * I2S (Microphone)
+ */
+#define PIN_I2S_SD 1
+#define PIN_I2S_WS 2
+#define PIN_I2S_SCK 42
+#define PIN_I2S_MCK 41
+
+/**
+ * Servos
+ */
+#define PIN_SERVO_PAN 3
+#define PIN_SERVO_TILT 46
+
+/**
+ * Motors
+ */
+#define PIN_M1_IN1 45
+#define PIN_M1_IN2 48
+#define PIN_M1_ENCA 19
+#define PIN_M1_ENCB 47
+
+#define PIN_M2_IN1 39
+#define PIN_M2_IN2 40
+#define PIN_M2_ENCA 35
+#define PIN_M2_ENCB 36
+
+/**
+ * Other peripherals
+ */
+#define PIN_LED_WS2812 48
+#define PIN_IR_LED 21
