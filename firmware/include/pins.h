c1f4cd4225abec1c540eb07a2f7a189ef27cefb4
justinl08 <103504232+justinl08@users.noreply.github.com>
2025-08-21 16:59:46 +1000

    Add pin definitions for ESP-CAM module

--- firmware/include/pins.h
+++ firmware/include/pins.h
@@ -1,6 +1,7 @@
 /**
  * Camera
  */
+#ifdef esp32s3cam_lonelybinary
 #define PIN_CAM_PWDN -1 // not connected
 #define PIN_CAM_RESET -1 // not connected
 
@@ -20,24 +21,60 @@
 #define PIN_CAM_D2 8  // CAM_Y4
 #define PIN_CAM_D1 9  // CAM_Y3
 #define PIN_CAM_D0 11 // CAM_Y2
+#endif
+
+#ifdef esp32cam_aithinker
+#define PIN_CAM_PWDN 32
+#define PIN_CAM_RESET -1 // not connected
+
+#define PIN_CAM_SIOD 26
+#define PIN_CAM_SIOC 27
+
+#define PIN_CAM_VSYNC 25
+#define PIN_CAM_HREF 23
+#define PIN_CAM_XCLK 0
+#define PIN_CAM_PCLK 22
+
+#define PIN_CAM_D7 35 // CAM_Y9
+#define PIN_CAM_D6 34 // CAM_Y8
+#define PIN_CAM_D5 39 // CAM_Y7
+#define PIN_CAM_D4 36 // CAM_Y6
+#define PIN_CAM_D3 21 // CAM_Y5
+#define PIN_CAM_D2 19 // CAM_Y4
+#define PIN_CAM_D1 18 // CAM_Y3
+#define PIN_CAM_D0 5  // CAM_Y2
+
+#define PIN_CAM_FLASH 4 // Onboard flash on the ESP32CAM
+#endif
 
 /**
  * I2S (Microphone)
  */
+#ifdef esp32s3cam_lonelybinary
 #define PIN_I2S_SD 1
 #define PIN_I2S_WS 2
 #define PIN_I2S_SCK 42
 #define PIN_I2S_MCK 41
+#endif
+
+#ifdef esp32cam_aithinker
+#define PIN_I2S_SD  13 // D13 / IO13
+#define PIN_I2S_WS  14 // D14 / IO14
+#define PIN_I2S_SCK 16 // RX2 / IO16
+#endif
 
 /**
  * Servos
  */
+#ifdef esp32s3cam_lonelybinary
 #define PIN_SERVO_PAN 3
 #define PIN_SERVO_TILT 46
+#endif
 
 /**
  * Motors
  */
+#ifdef esp32s3cam_lonelybinary
 #define PIN_M1_IN1 45
 #define PIN_M1_IN2 48
 #define PIN_M1_ENCA 19
@@ -47,9 +84,12 @@
 #define PIN_M2_IN2 40
 #define PIN_M2_ENCA 35
 #define PIN_M2_ENCB 36
+#endif
 
 /**
  * Other peripherals
  */
+#ifdef esp32s3cam_lonelybinary
 #define PIN_LED_WS2812 48
 #define PIN_IR_LED 21
+#endif
